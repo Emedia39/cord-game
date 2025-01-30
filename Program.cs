fc5e0b9758bb5612f14e52f9e79507758e1d29ba
Emedia39 <241207@st.yoshida-g.ac.jp>
2025-01-30 15:39:31 +0900

    4枚のカード、1～4でそれぞれの重複数をカウントする

--- Program.cs
+++ Program.cs
@@ -16,6 +16,8 @@ namespace Card_Game
             //文字列inputの数値への変換を試行用
             int[] card = new int[4];
 
+
+
             Console.WriteLine("+------------------+");
             Console.WriteLine("|ポーカードゲーム！|");
             Console.WriteLine("+------------------+");
@@ -52,6 +54,53 @@ namespace Card_Game
 
             //キー入力待ち
             Console.ReadLine();
+
+            /*
+            方法1.1～4の「card」をすべて「number」-「number」して0になったのが、一番多い数をペア数にする
+            方法2.1～4の数を「number」に当てはめ、複数同じ数があったら、一番多い数をペア数にする←こっち
+             */
+
+            int[] serectCount = new int[4];//重複数カウント用配列
+            for (int number = 0; number < 4; number++)
+            {
+                serectCount[number] = 0;//初期値
+            }
+
+            for (int number = 0; number < 4; number++)
+            {
+                if(card[number] == 1)
+                {
+                    serectCount[0]++;
+                }
+                if (card[number] == 2)
+                {
+                    serectCount[1]++;
+                }
+                if (card[number] == 3)
+                {
+                    serectCount[2]++;
+                }
+                if (card[number] == 4)
+                {
+                    serectCount[3]++;
+                }
+
+                /*if (card[number] == number+1)//失敗
+                {
+                    serectCount[number] += 1;
+                }*/
+
+            }
+
+            Console.WriteLine(serectCount[0]);
+            Console.WriteLine(serectCount[1]);
+            Console.WriteLine(serectCount[2]);
+            Console.WriteLine(serectCount[3]);
+
+            //キー入力待ち
+            Console.ReadLine();
+
+
 
         }
     }
